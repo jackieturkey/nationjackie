b63c673851b1d5db8bc95a3ec3acffa956347eec
Jackie <jijiawei@shadt.com>
2018-01-04 10:31:04 +0800

    新增控制器

---
+++ app/Http/Controllers/TeacherController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/1/4
+ * Time: 10:26
+ */
+namespace App\Http\Controllers;
+class TeacherController extends Controller
+{
+    public function index()
+    {
+        return 'no';
+    }
+}
